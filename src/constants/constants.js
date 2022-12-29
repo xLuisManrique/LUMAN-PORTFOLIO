bc0e44e6b4a942637bdd3cbf52515fc964a1b0ea
Luis Fernando Manrique Becoche <102887322+xLuisManrique@users.noreply.github.com>
2022-12-29 16:34:38 -0500

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 export const projects = [
   {
     title: 'LUMAN IA',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    description: "Using React with an elegant user interface that resembles the ChatGPT app, communication with advanced GPT3 model API, and most importantly, the ability to ask the AI for help regarding JavaScript, React, or any other programming language, giving it code and translating it to another programming language, and much more, this CodeGPT is the best AI-based web application ",
       image: 'https://firebasestorage.googleapis.com/v0/b/luman-developer.appspot.com/o/IA-LUMAN.png?alt=media&token=ef7e7f92-c749-4589-ac38-edd09b2f55d7',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
+      tags: ['React'],
     source: 'https://github.com/xLuisManrique/LUMAN-IA-PROJECT',
     visit: 'https://luman-ia.vercel.app/',
     id: 0,
