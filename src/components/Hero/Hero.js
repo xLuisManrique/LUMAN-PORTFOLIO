7ba62b6eaf8361c505358ec4c0c9f1782243f1b5
Luis Fernando Manrique Becoche <102887322+xLuisManrique@users.noreply.github.com>
2023-08-22 08:02:48 -0500

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,12 +10,15 @@ const Hero = () => (
       <LeftSection>
         <SectionTitle main center>
           Welcome To <br />
-          LUMAN-FullStack Portfolio
+          My Portfolio Website!
         </SectionTitle>
         <SectionText>
-        The purpose of LUMAN is to take their development skills to the next level and build awesome apps.
+        I am Luis Manrique, a Developer Full Stack! My passion for development begins in the year 2022
+        acquiring skills in Python, JavaScript, PHP and many more tools that you can find in my github
+        repository.
         </SectionText>
         <Button onClick={() => window.location = 'https://github.com/xLuisManrique'}>Repository</Button>
+        <Button onClick={() => window.location = 'https://drive.google.com/file/d/1XSWJL2QzCGTiEb_srUA0Ti3j__y0-A3E/view?usp=sharing'}>My CV</Button>     
       </LeftSection>
     </Section>
   </>
