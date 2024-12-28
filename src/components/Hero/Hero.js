c4d244a3d15ecd0d225a91198ba9b00a8ce4c570
Luis Fernando Manrique Becoche <102887322+xLuisManrique@users.noreply.github.com>
2024-12-28 17:27:49 +0100

    Update Hero.js
    actualizacion descripción y cv

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,15 +10,16 @@ const Hero = () => (
       <LeftSection>
         <SectionTitle main center>
           Welcome To <br />
-          My Portfolio Website!
+          LUMAN Portfolio Website!
         </SectionTitle>
         <SectionText>
-        I am Luis Manrique, a Developer Full Stack! My passion for development begins in the year 2022
-        acquiring skills in Python, JavaScript, PHP and many more tools that you can find in my github
-        repository.
+        Luman is a Junior Developer Full Stack! with solid knowledge in creating web applications, both BackEnd and FrontEnd client.
+        My passion for development begins in the year 2023 acquiring skills in Python, JAVA, Pyhton, Javascript, TypeScript, 
+        Angular, PHP, Laravel, Symfony, NodeJS, MongoDB, SQL, and many others. tools that you can find in my github repository.
+        This allows Luis to have a complete profile, with the best coding practices.
         </SectionText>
         <Button onClick={() => window.location = 'https://github.com/xLuisManrique'}>Repository</Button>
-        <Button onClick={() => window.location = 'https://drive.google.com/file/d/1XSWJL2QzCGTiEb_srUA0Ti3j__y0-A3E/view?usp=sharing'}>My CV</Button>   
+        <Button onClick={() => window.location = 'https://drive.google.com/file/d/1aGo6RzWXyQHOUmRoHs4zFAi-orGxM4P1/view?usp=drive_link'}>CV</Button>   
       </LeftSection>
     </Section>
   </>
